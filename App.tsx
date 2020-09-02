8a5dc3ed912cb056d8ea37d42b75a1cd1bbc79be
Mauricio <mcimauricio.coelho@gmail.com>
2020-09-02 12:36:15 +0200

    Setup firebase and fetch comments

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as firebase from "firebase";
+import "firebase/firestore";
 import ApolloProvider from "./src/ApolloProvider";
 import HomeScreen from "./src/HomeScreen";
 import CommentsScreen from "./src/CommentsScreen";
 
+// TODO: make it safe
+const firebaseConfig = {
+  apiKey: "AIzaSyCzPLgFO4WhC3LAPuN9dFOLmWArNEeyGtA",
+  authDomain: "turtle-movies-b9c68.firebaseapp.com",
+  databaseURL: "https://turtle-movies-b9c68.firebaseio.com",
+  projectId: "turtle-movies-b9c68",
+  storageBucket: "turtle-movies-b9c68.appspot.com",
+  messagingSenderId: "156670765937",
+  appId: "1:156670765937:web:2b26a2ad6272a76ed4a4d0",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 export type RootStackParamList = {
   Movies: undefined;
-  Comments: { title: string };
+  Comments: { title: string; year: string };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
